161473b5f5c599b7025c36b5654a9895bb058fb4
Sophie <queensoph777@gmail.com>
2023-05-29 14:51:55 -0500

    Finished

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
 
+function generateTarget() {
+  return Math.floor(Math.random() * 10)
+}
+
+function compareGuesses(human, cpu, target) {
+
+    let humanGuess = Math.abs(target - human);
+    let cpuGuess = Math.abs(target - cpu);
+
+    if (humanGuess === cpuGuess) {
+        return true;
+    } else if (humanGuess > cpuGuess) {
+        return true;
+    } else return false;
+}
+
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore++;
+    } else {
+        computerScore++
+    }
+
+    function advanceRound() {
+        currentRoundNumber++;
+    }
